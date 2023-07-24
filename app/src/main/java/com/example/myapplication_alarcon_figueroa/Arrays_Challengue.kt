4ab7143ceb71e48f701482ac39b72478ecd8e6ea
Daniel <dsalarcon3@soy.sena.edu.co>
2023-07-23 21:43:40 -0500

    Challenge done

--- app/src/main/java/com/example/myapplication_alarcon_figueroa/Arrays_Challengue.kt
+++ app/src/main/java/com/example/myapplication_alarcon_figueroa/Arrays_Challengue.kt
@@ -5,35 +5,102 @@ class Arrays_Challengue {
 
 fun main() {
 
-    val cars = mutableListOf("Chevrolet","Mazda","Toyota")
-    println("desea ingresar un nuevo auto? ")
-    var choice:String = readln()!!.toString()
+    val cars = mutableListOf("Mazda")
+    var choice = "yes"
+
     while (choice=="yes"){
 
-        println("ingresa el nuevo auto: ")
-        var newCar:String = readLine()!!.toString()
-        cars.add(newCar)
-
-            println("desea buscar un auto? ")
-            choice = readln()!!.toString()
-
-            if (choice=="yes"){
-                print("ingrese el nombre del auto: ")
-                val nombre:String = readLine()!!.toString()
-
-                for (element in cars){
-                    if(nombre==element){
-                        println("el auto $nombre está en la lista y en la posición "+cars.lastIndexOf(nombre))
-                    }
-                    else{
-                        println("No se encuentra dentro de la lista")
-                    }
-
-                }
-                }
+                print("desea ver el menú de inicio?: ")
+                choice = readLine()!!.toString()
+                if (choice=="yes"){
+                    println("""
+                        1. Ingresar un nuevo auto
+                        2. Mostrar todos los autos
+                        3. Buscar un auto
+                        4. Modificar un auto
+                        5. Eliminar un auto
+                    """.trimIndent())
 
 
+                print("Qué acción desea realizar?, Ingrese el número correspondiente: ")
+                val menu:Int = readLine()!!.toInt()
+                 if (menu==1){
+                     println("ingresa el nuevo auto: ")
+                     var newCar:String = readLine()!!.toString()
+                     for ((index, element) in cars.withIndex()){
+                         if(newCar==element){
+                             println("el auto $element está en la lista y en la posición $index")
+                         }
+                     }
+                     print("Ingrese un nuevo auto: ")
+                     newCar= readLine()!!.toString()
+                     cars.add(newCar)
+                 }
+                    else if(menu==2){
+                        println("Los autos son: "+cars)
+                 }
+                    else if(menu==3){
+                     print("ingrese el nombre del auto: ")
+                     var nombre:String = readLine()!!.toString()
+
+                     for ((index, element) in cars.withIndex()){
+                         if(nombre==element){
+                             println("el auto $element está en la lista y en la posición $index")
+                         }
+                         else{
+                             println("$nombre No se encuentra dentro de la lista en la posición $index, ingrese otro nombre: ")
+                             nombre = readLine()!!.toString()
+                             if(nombre==element){
+                                 println("el auto $element está en la lista y en la posición $index")
+                             }
+                         }
+                     }
+                 }
+                    else if(menu==4){
+                        println("La lista de autos es esta: "+cars)
+                        print("ingrese el nombre del auto a modificar: ")
+                        var modiCar:String = readLine()!!.toString()
+
+                     for((index,element) in cars.withIndex()){
+                         if(modiCar==element){
+                             println("el auto $element con la posición $index será modificado, ingrese el nuevo nombre:  ")
+                             val newName:String = readLine()!!.toString()
+                             cars[index]=newName
+                             println("La lista ha sido modificada, y ahora quedaría así: "+cars)
+                         }
+                         else{
+                             println("$modiCar no existe y por lo tanto, no será modificado. ingrese un nombre existente: ")
+                             modiCar = readLine()!!.toString()
+                             println("$modiCar sí existe, ingrese el nuevo nombre: ")
+                             val newName2= readLine()!!.toString()
+                             cars[index]=newName2
+                             println("La lista ha sido modificada, y ahora quedaría así: "+cars)
+                         }
+                     }
+                 }
+                    else if (menu==5){
+                        println("Ingrese el nombre del auto a eliminar: ")
+                        var deleteCar:String= readLine()!!.toString()
+
+                     for((index,element) in cars.withIndex()){
+                         if (deleteCar==element){
+                             cars.remove(deleteCar)
+                             println("$deleteCar ha sido eliminado de la posición $index y la lista quedaria asi: "+cars)
+                         }
+                     }
+
+                     println("$deleteCar no existe y por lo tanto, no será eliminado. ingrese un nombre existente: ")
+                     deleteCar = readLine()!!.toString()
+                     println("$deleteCar sí existe, por lo tanto será eliminado, y la lista queda así: "+cars.remove(deleteCar)+cars)
+
+
+                 }
         choice+1
 
         }
+                else{
+                    println("Grascia por su tiempo")
+                }
+    }
+
 }
